b019892b3c2d18863f6f7aa6df1fa678bc1e10fc
myong003 <matthew.yong0801@gmail.com>
2022-12-09 02:14:22 -0800

    Finished pseudocode doesnt work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 class Ob:
     def __init__(self, f1, f2, f3, f4, f5, f6, f7):
@@ -11,7 +12,18 @@ class Ob:
         self.f7 = f7
 
 def GetDistance(ob1, ob2):
-    ob1.f1 - ob2.f2
+    # dist1 = (ob1.f1 - ob2.f1) * (ob1.f1 - ob2.f1)
+    # dist2 = (ob1.f2 - ob2.f2) * (ob1.f2 - ob2.f2)
+    dist3 = (float(ob1.f3) - float(ob2.f3)) * (float(ob1.f3) - float(ob2.f3))
+    dist4 = (float(ob1.f4) - float(ob2.f4)) * (float(ob1.f4) - float(ob2.f4))
+    dist5 = (float(ob1.f5) - float(ob2.f5)) * (float(ob1.f5) - float(ob2.f5))
+    dist6 = (float(ob1.f6) - float(ob2.f6)) * (float(ob1.f6) - float(ob2.f6))
+    dist7 = (float(ob1.f7) - float(ob2.f7)) * (float(ob1.f7) - float(ob2.f7))
+    # dist4 = (ob1.f4 - ob2.f4) * (ob1.f4 - ob2.f4)
+    # dist5 = (ob1.f5 - ob2.f5) * (ob1.f5 - ob2.f5)
+    # dist6 = (ob1.f6 - ob2.f6) * (ob1.f6 - ob2.f6)
+    # dist7 = (ob1.f7 - ob2.f7) * (ob1.f7 - ob2.f7)
+    return math.sqrt(dist3 + dist4 + dist5 + dist6 + dist7)
 
 with open("CS170_Small_Data__1.txt") as file:
     lines = [line.rstrip() for line in file]
@@ -21,11 +33,25 @@ numCorrect = 0
 for i in range(len(lines)) :
     entry = lines[i].split()
     newOb = Ob(entry[0], entry[1], entry[2], entry[3], entry[4], entry[5], entry[6])
+    
 
-    distance = sys.maxsize
-    location = sys.maxsize
+    minDistance = sys.maxsize
+    minLocation = sys.maxsize
 
     for k in range(len(lines)) :
         if k != i:
             entry2 = lines[k].split()
             newOb2 = Ob(entry2[0], entry2[1], entry2[2], entry2[3], entry2[4], entry2[5], entry2[6])
+            distance = GetDistance(newOb, newOb2)
+            if distance < minDistance:
+                minDistance = distance
+                minLocation = k
+
+    # print("Object " + newOb.f1 + " is class " + newOb.f2)
+    # print("It's nearest neighbor is " + minLocation + " which is in class " + lines[minLocation][1])
+
+    if newOb.f2 == lines[minLocation][1]:
+        numCorrect += 1
+
+accuracy = numCorrect / len(lines)
+print(numCorrect)
